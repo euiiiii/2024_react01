0d22dbc22349d31df52a653b50b7428bc4ae2180
leeeuijin <kouy96@naver.com>
2024-11-18 17:01:23 +0900

    241118 17:01 커밋

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 import Courseitem from "./components/course/Courseitem";
 import CourseListCard from "./components/course/CourseListCard";
+import Library from "./components/step01/Library";
 
 /*
 function Header() {
@@ -42,14 +43,14 @@ function App() {
   return (
     // 리액트에서는 class는 예약어이기 때문에 사용 못하고,
     // class 대신 className이라고 사용한다.
+    <div>
+    {/*
     <main>
-      {/* 
+     
     <Header />
     <Main />
     <Footer /> 
-    */}
 
-      {/*
     <main>
 		<div class="card">
 			<div class="card__header">강의 목록</div>
@@ -80,8 +81,7 @@ function App() {
 			</div>
 		</div>
 	</main>
-  */}
-      {/*
+
 		  <div class="card">
 			  <div class="card__header">강의 목록</div>
 			  <div class="card__body">
@@ -94,9 +94,13 @@ function App() {
           3단계. 중복되는 부분을 List 형태로 전부 담을 수 있다.
       </div>
     </div>
-    </div> */}
+    </div> 
+    3단계. 중복되는 부분을 List 형태로 전부 담을 수 있다.
       <CourseListCard />
     </main>
+     */}
+      <Library />
+    </div>
   );
 }
 

81539e527fece56917d345c99a711637c22846d2
leeeuijin <kouy96@naver.com>
2024-11-19 14:52:45 +0900

    241119 14:52 조건부 랜더링

--- src/App.js
+++ src/App.js
@@ -49,17 +49,20 @@ function App() {
     {
       title: '입문자를 위한, HTML&CSS 웹 개발 입문',
       description: '웹 개발에 필요한 기본 지식을 배웁니다.',
-      img: './img/coffee-blue.jpg'
+      img: './img/coffee-blue.jpg',
+      isFavorite: true
     },
     {
       title: '입문자를 위한, ES6+ 최신 자바스크립트 입문',
       description: '쉽고! 알찬! 내용을 준비했습니다.',
-      img: './img/photo-2.jpg'
+      img: './img/photo-2.jpg',
+      isFavorite: false
     },
     {
       title: '포트폴리오 사이트 만들고 배포까지!',
       description: '포트폴리오 사이트를 만들고 배포해 보세요.',
-      img: './img/photo-3.jpg'
+      img: './img/photo-3.jpg',
+      isFavorite: true
     }
   ]
 
--- src/components/step04/Courseitem.jsx
+++ src/components/step04/Courseitem.jsx
@@ -1,13 +1,33 @@
-function CourseItem({title, description, img}) {
-    return(
-      <article class="course">
-      <img class="course__img" src={img} alt="강의 이미지" style={{width:100}}/>
-      <div class="course__body">
-        <div className="course__title">{title}</div>
-        <div className="course__description">{description}
-        </div>
-      </div>
-    </article>
+import React from 'react';
+
+function HeartIconBtn({ isFavorite = false }) {
+    if (isFavorite) {
+        return (
+            <button className='btn'>
+                <img className='btn_img' src='./img/heart-fill-icon.svg' />
+            </button>
+        )
+    }
+    return (
+        <button className='btn'>
+            <img className='btn_img' src='./img/heart-icon.svg' />
+        </button>
+    )
+}
+
+function CourseItem({ title, description, img, isFavorite }) {
+    return (
+        <article className="course">
+            <img className="course__img" src={img} alt='강의이미지' style={{ width: "100px" }} />
+            <div className="course__body">
+                <div className="course__title">{title}</div>
+                <div className="course__description">{description}</div>
+            </div>
+            <div className='course_icons'>
+                <HeartIconBtn isFavorite={isFavorite} />
+            </div>
+        </article>
     );
-  }
-  export default CourseItem;
+}
+
+export default CourseItem;
